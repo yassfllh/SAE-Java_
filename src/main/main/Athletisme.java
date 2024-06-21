ded97c7f8e5599510472ef055b11737300b96d20
yassfllh <156653114+yassfllh@users.noreply.github.com>
2024-06-21 10:50:23 +0200

    Delete src/main/main directory

--- src/main/main/Athletisme.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import main.Sport.SportName;
-
-public class Athletisme extends Sport {
-    public Athletisme(int nbJoueursMax) {
-        super(Sport.SportName.ATHLETISME.getDisplayName(), nbJoueursMax);
-    }
-}
--- src/main/main/Volleyball.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class Volleyball extends Sport {
-    public Volleyball(int nbJoueursMax) {
-        super(Sport.SportName.VOLLEYBALL.getDisplayName(), nbJoueursMax);
-    }
-}
